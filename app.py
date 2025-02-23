9ce3fb0c97dbabcc70b8bc5612c5dbd358604a19
Devin <threateninglyamusing@gmail.com>
2025-02-22 18:29:07 -0800

    -

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
 import json
 
 with open('characters.json','r') as f:
-    file = f.read()
+    file = json.load(f)
 
+with open('index.html','w') as f:
+    f.write('''<style>
+    body {
+        font-family: Verdana
+    }
+</style>
+''')
 
-
-print(file)
+for character in file["characters"]:
+    info = f'''
+<h1>{character['name']}</h1>
+<h2>{character['pronunciation']}</h2>
+<li>{character['species']}
+<li>{character['sex']}
+<div>{character['description']}'''
+    with open('index.html','a') as f:
+        f.write(f'''<ul>{info}</ul>''')
 
 # <h1>First Middle<sup>(optional)</sup> Last</h1>
 # <h2>[first]-[middle(optional)]-[last]</h2>
