28fa7b3e2719c1f51dd50059760bfa2cfcbbda4c
Devin <threateninglyamusing@gmail.com>
2025-02-22 19:49:36 -0800

    -

--- app.py
+++ app.py
@@ -3,26 +3,50 @@ import json
 with open('characters.json','r') as f:
     file = json.load(f)
 
-with open('index.html','w') as f:
-    f.write('''<style>
+style = '''<style>
     body {
-        font-family: Verdana
+        font-size: 2rem;
+        font-family: Verdana;
+        padding: 2em;
     }
-</style>
-''')
-
+</style>'''
+homePage = f'''{style}
+'''
 for character in file["characters"]:
+    languages = []
+    nameList = []
+    for i in character['description']['languages']:
+        languages.append(i)
+    spokenLanguages = f''
+    index = 0
+    for i in languages:
+        spokenLanguages += f'''<li>{languages[index]}</li>'''
+        if index != len(languages)-1:
+            spokenLanguages += '''
+                '''
+        index += 1
     info = f'''
-<h1>{character['name']}</h1>
-<h2>{character['pronunciation']}</h2>
-<li>{character['species']}
-<li>{character['sex']}
-<div>{character['description']}'''
-    with open('index.html','a') as f:
-        f.write(f'''<ul>{info}</ul>''')
-
-# <h1>First Middle<sup>(optional)</sup> Last</h1>
-# <h2>[first]-[middle(optional)]-[last]</h2>
-# <li>Species
-# <li>Sex
-# <div>Description</div>
+<a href='./index.html'>
+    <== Back</a>
+        <h1>{character['full']}</h1>
+        <h2>{character['pronunciation']}</h2>
+        <ul>
+            <li>Species: {character['species']}</li>
+            <li>Sex: {character['sex']}</li>
+            <li>JobL {character['description']['job']}</li>
+            <li>Nationality: {character['description']['nationality']}</li>
+            <li>Place of Birth: {character['description']['hometown']}</li>
+            <li>Spoken Languages:</li>
+            <ul>
+                {spokenLanguages}
+            </ul>
+        </ul>'''
+    nameList.append(character['name'])
+    with open(f'characters/{character['name'].lower()}.html','w') as f:
+        f.write(f'''{style}
+{info}''')
+    
+    homePage += f'''<li><a href='{character['name']}.html'>{character['full']}</a></li>
+'''
+    with open('index.html','w') as f:
+        f.write(homePage)
